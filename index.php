dd00fea432905718aab3bbe4d23623c11f10bb38
Emilio Vargas Millan <emiliovm@outlook.es>
2022-06-14 21:00:32 +0200

    del 1 al 3

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// 1, crea programas de opraciones con dos variables
+
+    /*
+    $firstNumber = 21;
+    $secondNumber = 20;
+
+    #suma
+    echo $firstNumber + $secondNumber;
+    #resta
+    echo $firstNumber - $secondNumber;
+    #multiplicacion
+    echo $firstNumber * $secondNumber;
+    #division
+    echo $firstNumber / $secondNumber;
+
+    */
+
+// 2, crear un array e imprimir un elemento del array
+
+    /*
+
+    $array1 = ["hola","como",18,"estas"];
+
+    echo $array1[0];
+
+    */
+
+// 3, crear un array de arrays e imprimir un elemento del array
+
+    /*
+
+    $arrayMultiple = [[1,2,3],[4,5,6]];
+
+    // para ver todo el array print_r($arrayMultiple);
+
+    echo($arrayMultiple[1][0]);
+
+    */
+?>
